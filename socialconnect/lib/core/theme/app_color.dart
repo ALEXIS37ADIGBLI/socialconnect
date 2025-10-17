6d70ea463dcefdfe7f240590eb9b766af0caf534
OKE Komlan Erwin <okekomerwin@gmail.com>
2025-10-17 11:43:47 +0000

    fix: add color palette

---
+++ socialconnect/lib/core/theme/app_color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color.fromRGBO(31, 132, 137, 0);
+  static const backgroung = Color.fromRGBO(255, 255, 255, 0);
+  static const text = Color.fromRGBO(0, 0, 0, 0);
+}
